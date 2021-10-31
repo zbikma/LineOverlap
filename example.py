cdf46850535b232eba2b809274b373a4cf209c16
z.mazadi@gmail.com <zeinab.mazadi@mosaicco.com>
2021-10-31 14:40:49 -0600

    updated locally

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
-impot matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # this is a line that does not execute it is how we make commnets in python
+df=pd.read_csv("test.csv")
